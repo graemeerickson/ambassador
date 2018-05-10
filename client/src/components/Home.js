00eccf1c5c1afebf97b68a9ffbbb233b9529b3b3
graemeerickson <graemeerickson@gmail.com>
2018-05-09 17:44:24 -0700

    Added links to Ambassador and Homebuyer Registration screens. Added check to render a different screen depending on whether user is already logged in.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
+    if (this.props.user) { 
+      return(
+        <div>
+          <br/>
+          <p>Home screen for user who is already logged in</p>
+        </div>
+      );
+    }
     return(
-      <p>Home page</p>
+      <div>
+        <br/>
+        <h2>Sign up as:</h2>
+        <Link to="/ambassador-registration"><button className="btn btn-primary role-button">Neighborhood Ambassador</button></Link>
+        <Link to="/homebuyer-registration"><button className="btn btn-primary role-button">Prospective Home-buyer</button></Link>
+      </div>
     );
   }
 }
