f54f2f9f602ffacd70acf8085123745f58e96497
graemeerickson <graemeerickson@gmail.com>
2018-05-09 17:47:24 -0700

    Initial version of Homebuyer Registration form. Not yet functional.

---
+++ client/src/components/HomebuyerRegistration.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HomebuyerRegistration extends Component {
+  render() {
+    return(
+      <p>Homebuyer Registration form goes here</p>
+    )
+  }
+}
+
+export default HomebuyerRegistration;
