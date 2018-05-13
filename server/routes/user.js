7b0c7888af1418a8de023e8c789f3c73500322f8
graemeerickson <graemeerickson@gmail.com>
2018-05-13 10:40:17 -0700

    Added PUT route to update prospective homebuyer's target city, state, and locationCoordinates

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,33 +4,37 @@ const router = express.Router();
 const db = require('../models/user');
 
 router.post('/', (req, res) => {
-  console.log('Received POST request');
+  console.log("Received '/user' POST request");
   console.log('req.body.user:',req.body.user);
   db.find({_id: req.body.user.id}, (err, user) => {
     res.send(user);
   })
-  // db.User.findById(res.locals.currentUser._id, function(err, user) {
-  //   const userRecipe = user.recipes.filter( (recipe) => {
-  //     return recipe._id == recipeId;
-  //   })
-  //   console.log('userRecipe:', userRecipe);
-  //   res.render('viewRecipe', {userRecipe: userRecipe});
-  // })
 })
 
-router.get('/:id', (req, res) => {
-  console.log('Received GET request');
+router.get('/', (req, res) => {
+  console.log("Received '/user' GET request");
   console.log('params:',req.params.id);
+  db.find({}, (err, users) => {
+    res.send(users);
+  })
+});
+
+router.get('/:id', (req, res) => {
+  console.log("Received 'user/:id' GET request");
   db.findById(req.params.id, (err, user) => {
     res.send(user);
   })
-  // db.User.findById(res.locals.currentUser._id, function(err, user) {
-  //   const userRecipe = user.recipes.filter( (recipe) => {
-  //     return recipe._id == recipeId;
-  //   })
-  //   console.log('userRecipe:', userRecipe);
-  //   res.render('viewRecipe', {userRecipe: userRecipe});
-  // })
+})
+
+// update prospective homebuyer's target city, state, & location coordinates
+router.put('/:id', (req, res) => {
+  console.log("Received 'user/:id' PUT request");
+  console.log('req.params:', req.params)
+  console.log('req.body:', req.body)
+  db.findByIdAndUpdate(req.params.id, {$set:req.body}, (err, user) => {
+    if (err) { res.status(403).send('PUT request failed') };
+    res.send(user);
+  })
 })
 
 module.exports = router;
