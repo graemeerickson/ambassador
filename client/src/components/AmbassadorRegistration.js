46cda3a7915567b252435ec0129918c19f8cb536
graemeerickson <graemeerickson@gmail.com>
2018-05-13 10:27:56 -0700

    Replaced homeAddressCoordinates with more generic locationCoordinates.

--- client/src/components/AmbassadorRegistration.js
+++ client/src/components/AmbassadorRegistration.js
@@ -16,7 +16,7 @@ class AmbassadorRegistration extends Component {
       homeAddressCity: '',
       homeAddressState: '',
       homeAddressZip: '',
-      homeAddressCoordinates: null,
+      locationCoordinates: null,
       role: 'Neighborhood Ambassador',
       targetZip: ''
     };
@@ -43,7 +43,7 @@ class AmbassadorRegistration extends Component {
     axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${homeAddressStreetTransformed}+${homeAddressCityTransformed},+${homeAddressStateTransformed}+${homeAddressZipTransformed}&key=${GOOGLEMAPS_API_KEY}`)
       .then(res => {
         console.log('Successfully reached Google Maps API:', res);
-        this.setState({ homeAddressCoordinates: [res.data.results[0].geometry.location.lng, res.data.results[0].geometry.location.lat] }, () => {
+        this.setState({ locationCoordinates: [res.data.results[0].geometry.location.lng, res.data.results[0].geometry.location.lat] }, () => {
           axios.post('http://localhost:3001/auth/signup', this.state)
           .then(result => {
             console.log('Successfully added user to db:', result);
@@ -67,11 +67,11 @@ class AmbassadorRegistration extends Component {
         <br />
         <form onSubmit={this.handleSubmit}>
           <div className="form-group form-inline">
-            <label htmlFor="firstName">First Name</label>
+            <label htmlFor="firstName">First name</label>
             <input name="firstName" type="text" className="form-control" placeholder="Johnny" value={this.state.firstName} onChange={this.handleFirstNameChange} />
           </div>
           <div className="form-group form-inline">
-            <label htmlFor="lastName">Last Name</label>
+            <label htmlFor="lastName">Last name</label>
             <input name="lastName" type="text" className="form-control" placeholder="Appleseed" value={this.state.lastName} onChange={this.handleLastNameChange} />
           </div>
           <div className="form-group form-inline">
@@ -83,11 +83,11 @@ class AmbassadorRegistration extends Component {
             <input name="password" type="password" className="form-control" value={this.state.password} onChange={this.handlePasswordChange} />
           </div>
           <div className="form-group form-inline">
-            <label htmlFor="phoneNumber">Phone Number</label>
+            <label htmlFor="phoneNumber">Phone number</label>
             <input name="phoneNumber" type="text" className="form-control" placeholder="555-555-5555" value={this.state.phoneNumber} onChange={this.handlePhoneNumberChange} />
           </div>
           <div className="form-group">
-            <label htmlFor="homeAddress">Home Address</label>
+            <label htmlFor="homeAddress">Home address</label>
             <input name="homeAddressStreet" type="text" className="form-control" placeholder="123 Main Street" value={this.state.homeAddressStreet} onChange={this.handleHomeAddressStreetChange} />
             <input name="homeAddressCity" type="text" className="form-control" placeholder="Seattle" value={this.state.homeAddressCity} onChange={this.handleHomeAddressCityChange} />
             <input name="homeAddressState" type="text" className="form-control" placeholder="WA" value={this.state.homeAddressState} onChange={this.handleHomeAddressStateChange} />
