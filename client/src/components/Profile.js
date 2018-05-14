829ac79c7fe1c7d1c8dbc952eec31a7545f96bbe
graemeerickson <graemeerickson@gmail.com>
2018-05-13 19:59:42 -0700

    Revised to show info dependent on whether user is an ambassador or homebuyer

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -3,33 +3,54 @@ import React, { Component } from 'react';
 class Profile extends Component {
   render() {
     if (this.props.user) {
-      return (
+      if (this.props.user.role[0] === 'Prospective Homebuyer') {
+        return (
+          <div>
+            <br/>
+            <h1>Hi, {this.props.user.firstName}</h1>
+            <h5>Role: {this.props.user.role[0]}</h5>
+            <h5>Desired Location</h5>
+            <p className="profile-paragraph">
+              {this.props.user.targetAddress}
+            </p>
+            <h5>Contact Information</h5>
+            <p className="profile-paragraph">
+              Email: {this.props.user.email}<br/>
+              Phone: {this.props.user.phoneNumber}
+            </p>
+          </div>
+        );
+      }
+      else {
+        return (
+          <div>
+            <br/>
+            <h1>Hi, {this.props.user.firstName}</h1>
+            <h5>Role: {this.props.user.role[0]}</h5>
+            <h5>Home Address</h5>
+            <p className="profile-paragraph">
+              {this.props.user.homeAddressStreet}<br/>
+              {this.props.user.homeAddressCity},&nbsp;
+              {this.props.user.homeAddressState}&nbsp;
+              {this.props.user.homeAddressZip}
+            </p>
+            <h5>Contact Information</h5>
+            <p className="profile-paragraph">
+              Email: {this.props.user.email}<br/>
+              Phone: {this.props.user.phoneNumber}
+            </p>
+          </div>
+        );
+      }
+    }
+    else {
+      return(
         <div>
           <br/>
-          <h5>Name: {this.props.user.firstName}&nbsp;{this.props.user.lastName}</h5>
-          <h5>Role: {this.props.user.role[0]}</h5>
-          <h5>Home Address</h5>
-          <p className="profile-paragraph">
-            {this.props.user.homeAddressStreet}<br/>
-            {this.props.user.homeAddressCity},&nbsp;
-            {this.props.user.homeAddressState}&nbsp;
-            {this.props.user.homeAddressZip}
-          </p>
-          <h5>Contact Information</h5>
-          <p className="profile-paragraph">
-            Email: {this.props.user.email}<br/>
-            Phone: {this.props.user.phoneNumber}
-          </p>
+          <h6><a href="/login">Log in</a> or get started by signing up as a <a href="/AmbassadorRegistration">Neighborhood Ambassador</a> or a <a href="/HomebuyerRegistration">Prospective Homebuyer</a>.</h6>
         </div>
       );
     }
-
-    return(
-      <div>
-        <p>This is a profile page. You must be logged in to see it.</p>
-        <p>Would you like to <a href="/login">log in</a> or <a href="/signup">sign up</a>?</p>
-      </div>
-    );
   }
 }
 
