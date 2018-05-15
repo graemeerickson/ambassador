d65c9965b675551aa32fe61816bf7578f88148c7
graemeerickson <graemeerickson@gmail.com>
2018-05-15 16:48:41 -0700

    Added comment to describe route

--- server/routes/ambassadors.js
+++ server/routes/ambassadors.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const db = require('../models/user');
 
+// get all users from the db whose role is 'Neighborhood Ambassador'
 router.get('/', (req, res) => {
-  console.log("Received '/' GET request");
+  console.log("Received '/ambassadors' GET request");
   db.find({}, (err, users) => {
     let ambassadors = [];
     users.forEach( ambassador => {
