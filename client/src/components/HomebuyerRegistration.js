bd40b603fe692e22ac4e0706d6570855f802a850
graemeerickson <graemeerickson@gmail.com>
2018-05-15 16:35:45 -0700

    Removed console.logs that exposed user data

--- client/src/components/HomebuyerRegistration.js
+++ client/src/components/HomebuyerRegistration.js
@@ -28,16 +28,16 @@ class HomebuyerRegistration extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Form submitted:', this.state);
+    console.log('Form submitted');
     // pass address to Google Maps API to retrieve long/lat coordinates before adding user to db
     let targetAddressTransformed = this.state.targetAddress.replace(' ','+');
     axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${targetAddressTransformed}&key=${GOOGLEMAPS_API_KEY}`)
       .then(res => {
-        console.log('Successfully reached Google Maps API:', res);
+        console.log('Successfully reached Google Maps API');
         this.setState({ locationCoordinates: [res.data.results[0].geometry.location.lng, res.data.results[0].geometry.location.lat] }, () => {
           axios.post(SERVER_URL + '/auth/signup', this.state)
           .then(result => {
-            console.log('Successfully added user to db:', result);
+            console.log('Successfully added user to db');
             // add newly-received token to localStorage
             localStorage.setItem('loginToken', result.data.token);
             // update user with a call to App.js
