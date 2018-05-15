f4553b6df7b67d67793c08f79f6b99948cde2acf
graemeerickson <graemeerickson@gmail.com>
2018-05-15 16:37:51 -0700

    Removed console.logs that exposed user data

--- client/src/components/HomebuyerTargetLocation.js
+++ client/src/components/HomebuyerTargetLocation.js
@@ -16,15 +16,15 @@ class HomebuyerTargetLocation extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Form submitted:', this.state)
+    console.log('Form submitted')
     let targetAddressTransformed = this.state.targetAddress.replace(' ','+');
     axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${targetAddressTransformed}&key=${GOOGLEMAPS_API_KEY}`)
       .then(res => {
-        console.log('Successfully reached Google Maps API:', res);
+        console.log('Successfully reached Google Maps API');
         this.setState({ locationCoordinates: [res.data.results[0].geometry.location.lng, res.data.results[0].geometry.location.lat] }, () => {
           axios.put(SERVER_URL + `/user/${this.props.user.id}`, this.state)
             .then(res => {
-              console.log('PUT request succeeded:', res)
+              console.log('PUT request succeeded')
               this.props.updateTargetLocation();
             })
             .catch(err => {
