1ad1e9b7c112488d6916f058886077036197fa67
graemeerickson <graemeerickson@gmail.com>
2018-05-15 16:27:30 -0700

    Removed unused classnames module & related references

--- client/src/layout/Nav.js
+++ client/src/layout/Nav.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
-import classnames from 'classnames';
 
 class Nav extends Component {
   constructor(props) {
@@ -22,7 +21,6 @@ class Nav extends Component {
 
   render() {
     let links = '';
-    let classes = classnames('specialbutton', {active: this.state.active});
     if (this.props.user) {
       links = (
         <span className="nav-link">
