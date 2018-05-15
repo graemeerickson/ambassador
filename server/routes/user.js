acb7dd2712150de9afce199039f6c214ded8f33c
graemeerickson <graemeerickson@gmail.com>
2018-05-15 16:52:58 -0700

    Added description comments. Removed unneeded console.logs.

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,20 +5,20 @@ const db = require('../models/user');
 
 router.post('/', (req, res) => {
   console.log("Received '/user' POST request");
-  console.log('req.body.user:',req.body.user);
   db.find({_id: req.body.user.id}, (err, user) => {
     res.send(user);
   })
 })
 
+// get all users from db
 router.get('/', (req, res) => {
   console.log("Received '/user' GET request");
-  console.log('params:',req.params.id);
   db.find({}, (err, users) => {
     res.send(users);
   })
 });
 
+// get specific user by ID from db
 router.get('/:id', (req, res) => {
   console.log("Received 'user/:id' GET request");
   db.findById(req.params.id, (err, user) => {
@@ -29,8 +29,6 @@ router.get('/:id', (req, res) => {
 // update prospective homebuyer's target city, state, & location coordinates
 router.put('/:id', (req, res) => {
   console.log("Received 'user/:id' PUT request");
-  console.log('req.params:', req.params)
-  console.log('req.body:', req.body)
   db.findByIdAndUpdate(req.params.id, {$set:req.body}, (err, user) => {
     if (err) { res.status(403).send('PUT request failed') };
     res.send(user);
