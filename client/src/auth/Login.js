80f8befd54f9d24e88e3ee9f2c8b656dcce47776
graemeerickson <graemeerickson@gmail.com>
2018-05-15 01:25:17 -0700

    Updated form styling. Added constant for server URL.

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
+import { SERVER_URL } from '../constants';
 
 class Login extends Component {
   constructor(props) {
@@ -16,7 +17,7 @@ class Login extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:3001/auth/login', this.state)
+    axios.post(SERVER_URL + '/auth/login', this.state)
     .then(result => {
       console.log('Success:', result);
       // add newly-received token to localStorage
@@ -37,13 +38,17 @@ class Login extends Component {
         <br/>
         <h2>Log into your account</h2><br/>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group form-inline">
-            <label htmlFor="email">Email address: </label>
-            <input name="Email" className="form-control" placeholder="johnny@appleseed.com" value={this.state.email} onChange={this.handleEmailChange} />
+          <div className="input-group mb-3 registration-input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="email">Email</span>
+            </div>
+            <input name="email" type="email" className="form-control" placeholder="johnny@appleseed.com" aria-label="Email" aria-describedby="email" value={this.state.email} onChange={this.handleEmailChange} required />
           </div>
-          <div className="form-group form-inline">
-            <label htmlFor="password">Password: </label>
-            <input name="Password" className="form-control" type="password" value={this.state.password} onChange={this.handlePasswordChange} />
+          <div className="input-group mb-3 registration-input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="password">Password</span>
+            </div>
+            <input name="password" type="password" className="form-control" aria-label="Password" aria-describedby="password" value={this.state.password} onChange={this.handlePasswordChange} required />
           </div>
           <input type="submit" value="Login" className="btn btn-primary" />
         </form>
