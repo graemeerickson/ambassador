2f5c7972e7046fd80ff36b0a057390e465824bfe
graemeerickson <graemeerickson@gmail.com>
2018-05-16 14:33:17 -0700

    Minor updates

--- client/src/components/HomebuyerRegistration.js
+++ client/src/components/HomebuyerRegistration.js
@@ -33,7 +33,7 @@ class HomebuyerRegistration extends Component {
     let targetAddressTransformed = this.state.targetAddress.replace(' ','+');
     axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${targetAddressTransformed}&key=${GOOGLEMAPS_API_KEY}`)
       .then(res => {
-        console.log('Successfully reached Google Maps API');
+        console.log('Successfully reached Google Maps API', res.data.results);
         this.setState({ locationCoordinates: [res.data.results[0].geometry.location.lng, res.data.results[0].geometry.location.lat] }, () => {
           axios.post(SERVER_URL + '/auth/signup', this.state)
           .then(result => {
