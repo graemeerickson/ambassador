9edb0ebebe7ef2e8504f47e38b13753fa86afee9
graemeerickson <graemeerickson@gmail.com>
2018-09-19 01:13:36 -0700

    Optimized db query for retrieving all neighborhood ambassadors

--- server/routes/ambassadors.js
+++ server/routes/ambassadors.js
@@ -5,13 +5,7 @@ const db = require('../models/user');
 
 // get all users from the db whose role is 'Neighborhood Ambassador'
 router.get('/', (req, res) => {
-  db.find({}, (err, users) => {
-    let ambassadors = [];
-    users.forEach( ambassador => {
-      if(ambassador.role[0] === 'Neighborhood Ambassador') {
-        ambassadors.push(ambassador);
-      }
-    })
+  db.find({ role: 'Neighborhood Ambassador' }, (err, ambassadors) => {
     res.send(ambassadors);
   })
 })
