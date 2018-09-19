262ebd7f395bca52514f7656c5d44133c5494038
graemeerickson <graemeerickson@gmail.com>
2018-09-18 21:03:12 -0700

    Removed console logs

--- server/routes/ambassadors.js
+++ server/routes/ambassadors.js
@@ -5,7 +5,6 @@ const db = require('../models/user');
 
 // get all users from the db whose role is 'Neighborhood Ambassador'
 router.get('/', (req, res) => {
-  console.log("Received '/ambassadors' GET request");
   db.find({}, (err, users) => {
     let ambassadors = [];
     users.forEach( ambassador => {
