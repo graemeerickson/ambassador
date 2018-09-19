170d5998b8284d191d04b498179d8515411a8378
graemeerickson <graemeerickson@gmail.com>
2018-09-18 21:28:12 -0700

    Converted Profile component from a class component to functional stateless component

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,57 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Profile extends Component {
-  render() {
-    if (this.props.user) {
-      if (this.props.user.role[0] === 'Prospective Homebuyer') {
-        return (
-          <div>
-            <br/>
-            <h1>Hi, {this.props.user.firstName}</h1><br/>
-            <h5>Role: {this.props.user.role[0]}</h5>
-            <h5>Desired Location</h5>
-            <p className="profile-paragraph">
-              {this.props.user.targetAddress}
-            </p>
-            <h5>Contact Information</h5>
-            <p className="profile-paragraph">
-              Email: {this.props.user.email}<br/>
-              Phone: {this.props.user.phoneNumber}
-            </p>
-          </div>
-        );
-      }
-      else {
-        return (
-          <div>
-            <br/>
-            <h1>Hi, {this.props.user.firstName}</h1><br/>
-            <h5>Role: {this.props.user.role[0]}</h5><br/>
-            <h5>Home Address</h5>
-            <p className="profile-paragraph">
-              {this.props.user.homeAddressStreet}<br/>
-              {this.props.user.homeAddressCity},&nbsp;
-              {this.props.user.homeAddressState}&nbsp;
-              {this.props.user.homeAddressZip}
-            </p>
-            <h5>Contact Information</h5>
-            <p className="profile-paragraph">
-              Email: {this.props.user.email}<br/>
-              Phone: {this.props.user.phoneNumber}
-            </p>
-          </div>
-        );
-      }
+const Profile = ({ user }) => {
+  if (user) {
+    if (user.role[0] === 'Prospective Homebuyer') {
+      return (
+        <div>
+          <br/>
+          <h1>Hi, {user.firstName}</h1><br/>
+          <h5>Role: {user.role[0]}</h5>
+          <h5>Desired Location</h5>
+          <p className="profile-paragraph">
+            {user.targetAddress}
+          </p>
+          <h5>Contact Information</h5>
+          <p className="profile-paragraph">
+            Email: {user.email}<br/>
+            Phone: {user.phoneNumber}
+          </p>
+        </div>
+      );
     }
     else {
-      return(
+      return (
         <div>
-          <br/><br/>
-          <h5><a href="/login">Log in</a> or get started by signing up as a <a href="/ambassador-registration">Neighborhood Ambassador</a> or a <a href="/homebuyer-registration">Prospective Homebuyer</a>.</h5>
+          <br/>
+          <h1>Hi, {user.firstName}</h1><br/>
+          <h5>Role: {user.role[0]}</h5><br/>
+          <h5>Home Address</h5>
+          <p className="profile-paragraph">
+            {user.homeAddressStreet}<br/>
+            {user.homeAddressCity},&nbsp;
+            {user.homeAddressState}&nbsp;
+            {user.homeAddressZip}
+          </p>
+          <h5>Contact Information</h5>
+          <p className="profile-paragraph">
+            Email: {user.email}<br/>
+            Phone: {user.phoneNumber}
+          </p>
         </div>
       );
     }
   }
-}
+  else {
+    return(
+      <div>
+        <br/><br/>
+        <h5><a href="/login">Log in</a> or get started by signing up as a <a href="/ambassador-registration">Neighborhood Ambassador</a> or a <a href="/homebuyer-registration">Prospective Homebuyer</a>.</h5>
+      </div>
+    );
+  }
+};
 
 export default Profile;
